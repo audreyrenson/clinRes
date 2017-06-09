c9cff2e1d03d1f16df9328d0ea7fd3cfc6a6905a
Audrey Renson <audrey.o.renson@gmail.com>
2017-06-09 16:38:34 -0400

    no formatting of p-values in glm_table()

--- R/glm_table.R
+++ R/glm_table.R
@@ -2,12 +2,11 @@
 glm_table <- function(fit, measure="OR") {
   # this will give you a neatly formatted table with exponentiated betas
   # from a glm object. Note: does not work with interaction terms
-  options(scipen=9999)
+
 
   if(class(fit) != "zeroinfl") {
 
-    pvals <- ifelse(coef(summary(fit))[,4] < 0.0001, "< 0.0001",
-                    as.character(round(coef(summary(fit))[,4], digits=4)))
+    pvals <- round(coef(summary(fit))[,4], digits=4)
     df <- data.frame(
       round(exp(coef(fit)), digits=3),
       round(exp(confint(fit)),digits=3),
