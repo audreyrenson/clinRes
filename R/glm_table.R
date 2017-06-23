4a8f68a74aeb75579165b566be8bc80e21bb425a
Renson <rensoa01@nyumc.org>
2017-06-23 11:33:10 -0400

    fmt.glm_table() and glm_table(fmt=TRUE) added.

--- R/glm_table.R
+++ R/glm_table.R
@@ -1,8 +1,10 @@
 #' @export
-glm_table <- function(fit, digits=4, p.digits=digits+1, se="normal", intercept=FALSE,
-                      fun_coef = if(fit$family$link %in% c("log","logit")) exp else I) {
+glm_table <- function(fit, digits=4, p.digits=digits+1, se="normal", intercept=FALSE, fmt=FALSE,
+                      fun_coef = if(fit$family$link %in% c("log","logit")) exp else I,
+                      ...) {
   # this will give you a neatly formatted table with exponentiated betas
   # from a glm object. Note: does not work with interaction terms
+  # ... args passed to fmt.glm_table
 
   coefs <- if(se=="normal") glm_coefs else if(se=="robust") glm_robust_coefs
 
@@ -19,7 +21,8 @@ glm_table <- function(fit, digits=4, p.digits=digits+1, se="normal", intercept=F
   colnames(tbl_coefs) <- c(measure,"upr","lwr","p-value")
 
   if(!intercept) tbl_coefs <- tbl_coefs[-1,,drop=FALSE]
-  tbl_coefs
+
+  if(fmt) fmt.glm_table(tbl_coefs, digits=digits, p.digits=p.digits, ...) else tbl_coefs
 }
 
 glm_coefs <- function(fit) {
@@ -54,5 +57,25 @@ glm_robust_coefs <- function(fit) {
 
   tbl
 }
+
+
+fmt.glm_table <- function(glm_tbl, tbl_colnames = c(colnames(glm_tbl)[1], "95% CI", "p-value"),
+                          tbl_rownames = rownames(glm_tbl), digits=2, p.digits=digits+1,
+                          p_fmt = function(p) format.pval(round(as.numeric(p),p.digits), eps = .001),
+                          beta_fmt = function(b) format(as.numeric(b), digits=digits, nsmall=digits),
+                          ci_fmt = function(col1, col2) paste0(beta_fmt(col1), " to ", beta_fmt(col2)) ) {
+
+  beta <- beta_fmt(glm_tbl[,1])
+  ci   <- ci_fmt(glm_tbl[,2], glm_tbl[,3])
+  p    <- p_fmt(glm_tbl[,4])
+
+  tbl <- cbind(beta, ci, p)
+  colnames(tbl) <- tbl_colnames
+  rownames(tbl) <- tbl_rownames
+  tbl
+
+}
+
+
 
 
