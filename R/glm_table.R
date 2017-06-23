adca47e0cca445b7b583c07262d622fbe35eec93
Renson <rensoa01@nyumc.org>
2017-06-23 11:14:54 -0400

    glm_table now uses family and link to decide whether to exponentiate and what to put as the column name for the beta. glm_table.rp is replaced with se="robust" in glm_table.

--- R/glm_table.R
+++ R/glm_table.R
@@ -4,37 +4,55 @@ glm_table <- function(fit, digits=4, p.digits=digits+1, se="normal", intercept=F
   # this will give you a neatly formatted table with exponentiated betas
   # from a glm object. Note: does not work with interaction terms
 
+  coefs <- if(se=="normal") glm_coefs else if(se=="robust") glm_robust_coefs
+
+  tbl_coefs <- coefs(fit)
+
+  tbl_coefs[,1:3] <- round(fun_coef(tbl_coefs[,1:3]), digits)
+  tbl_coefs[,4] <- round(tbl_coefs[,4], p.digits)
+
+  measure <- if(fit$family$link=="logit") {"OR"
+  } else if(fit$family$link=="log" & fit$family$family=="binomial") {"RR"
+  } else if(fit$family$link=="log" & fit$family$family=="poisson") {"IRR"
+  } else if(fit$family$link=="identity") {"Beta"}
+
+  colnames(tbl_coefs) <- c(measure,"upr","lwr","p-value")
+
+  if(!intercept) tbl_coefs <- tbl_coefs[-1,,drop=FALSE]
+  tbl_coefs
+}
+
+glm_coefs <- function(fit) {
   if(!"zeroinfl" %in% class(fit)) {
 
-    pvals <- round(coef(summary(fit))[,4], digits=p.digits)
     tbl <- cbind(
-      round(fun_coef(coef(fit)), digits=digits),
-      round(fun_coef(confint(fit)),digits=digits),
-      pvals
+      coef(fit),
+      confint(fit),
+      coef(summary(fit))[,4]
     )
-
   } else if(fit$dist == "negbin") {
 
-    pvals_count <- ifelse(summary(fit)$coefficients$count[,4] < 0.0001, "< 0.0001",
-                          as.character(round(summary(fit)$coefficients$count[,4], digits=p.digits)))
-    pvals_zero <- ifelse(summary(fit)$coefficients$zero[,4] < 0.0001, "< 0.0001",
-                         as.character(round(summary(fit)$coefficients$zero[,4], digits=p.digits)))
+    pvals_count <- summary(fit)$coefficients$count[,4]
+    pvals_zero <- summary(fit)$coefficients$zero[,4]
 
     tbl <- cbind(
-      round(fun_coef(coef(fit)), digits=3),
-      round(fun_coef(confint(fit)),digits=3),
+      coef(fit),
+      confint(fit),
       c(pvals_count[1:length(pvals_count)-1], pvals_zero)
     )
   }
-
-  measure <- if(fit$family$link=="logit") {"OR"
-      } else if(fit$family$link=="log" & fit$family$family=="binomial") {"RR"
-      } else if(fit$family$link=="log" & fit$family$family=="poisson") {"IRR"
-      } else if(fit$family$link=="identity") {"Beta"}
-
-  colnames(tbl) <- c(measure,"upr","lwr","p-value")
-
-  if(!intercept) tbl <- tbl[-1,,drop=FALSE]
   tbl
 }
+
+glm_robust_coefs <- function(fit) {
+  cov.fit <- sandwich::vcovHC(fit, type="HC0")
+  std.err <- sqrt(diag(cov.fit))
+  tbl <- cbind(coef(fit),
+               coef(fit) - 1.96 * std.err,
+               coef(fit) + 1.96 * std.err,
+               2 * pnorm(abs(coef(fit)/std.err), lower.tail=FALSE))
+
+  tbl
+}
+
 
