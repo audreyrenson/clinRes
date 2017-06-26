5e3e7698feef3ba71e4980e7ddbb0be7c64ca321
Renson <rensoa01@nyumc.org>
2017-06-26 17:04:16 -0400

    moved fmt.glm_table to prettytable_functions.

--- R/prettytable_functions.R
+++ R/prettytable_functions.R
@@ -175,4 +175,64 @@ table_one <- function(vars, varlabels=vars, data, strata, normal=NULL, exact=NUL
 
 
 
+fmt.glm_table <- function(glm_tbl, tbl_colnames = c(colnames(glm_tbl)[1], "95% CI", "p-value"),
+                          tbl_rownames = rownames(glm_tbl), digits=2, p.digits=digits+1,
+                          p_fmt = function(p) format.pval(round(as.numeric(p),p.digits), eps = .001),
+                          beta_fmt = function(b) format(as.numeric(b), digits=digits, nsmall=digits),
+                          ci_fmt = function(col1, col2) paste0(beta_fmt(col1), " to ", beta_fmt(col2)) ) {
+
+  beta <- beta_fmt(glm_tbl[,1])
+  ci   <- ci_fmt(glm_tbl[,2], glm_tbl[,3])
+  p    <- p_fmt(glm_tbl[,4])
+
+  tbl <- cbind(beta, ci, p)
+  colnames(tbl) <- tbl_colnames
+  rownames(tbl) <- tbl_rownames
+  tbl
+
+}
+
+glm_table_indents <- function(fit, ref_txt="Ref", digits=4,
+                              ref_num=if(fit$family$link %in% c("log","logit")) 1 else 0,  ...) {
+
+  tbl <- glm_table(fit, digits = digits, ...)
+
+  ref_num <- format(as.numeric(ref_num), digits=digits, nsmall=digits)
+
+  INDENTS <- "&nbps"
+  BLANKROW <- rep("", ncol(tbl))
+  REFROW <- c(ref_num, rep(ref_txt, ncol(tbl)-1))
+
+  vars <- as.character(attr(fit$terms, "variables"))[c(-1,-2)]
+  dat <- fit$data[,vars, drop=FALSE]
+  var_classes <- sapply(vars, function(i) class(dat[,i]))
+
+  numeric_vars <- vars[var_classes %in% c("numeric","integer")]
+  factor_vars <- vars[var_classes %in% c("factor","character","logical")]
+
+  var_rownums <- sapply(vars, function(i) if(i %in% numeric_vars) 1 else length(fit$xlevels[[i]]) + 1 )
+  var_rownames <- sapply(vars, function(i) if(i %in% numeric_vars) i else c(i, fit$xlevels[[i]]))
+
+  list_tblrows <- vector("list", length = length(vars))
+  names(list_tblrows) <- vars
+
+  list_tblrows <- lapply(vars, function(i)
+    if(i %in% rownames(tbl)) tb <- tbl[i,,drop=FALSE] else {
+      tb <- rbind(BLANKROW, REFROW, tbl[paste0(i, var_rownames[[i]][c(-1,-2)]) ,])
+      rownames(tb) <- paste0(c("",rep(INDENTS, nrow(tb)-1)), var_rownames[[i]])
+      tb
+    }
+  )
+
+
+  list_tblrows
+
+  tbl_out <- do.call(rbind, list_tblrows)
+
+  tbl_out
+
+}
+
+
+
 
